f57b4e97b789b431f19d837bc9f54865fbed3aa2
emilianogastaldo <emiliano0303@gmail.com>
2024-02-22 15:35:05 +0100

    perché non funziona così?

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const { createApp } = Vue;
-
+const endpointPhp = 'http://localhost/php-todo-list-json/api/tasks/index.php';
+const endpointJson = 'http://localhost/php-todo-list-json/api/tasks.json';
 const app = createApp({
     name: 'Todo List',
+    data: () => ({
+        tasks: [],
+    }),
+    created() {
+        axios.get(endpointPhp).then(res => {
+            this.tasks = res.data;
+        })
+    }
 });
 
 app.mount('#app');
