49f57da168e00f8ce5e9e076854c3412ce6ce3fb
emilianogastaldo <emiliano0303@gmail.com>
2024-02-22 17:04:38 +0100

    add new task

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,8 +1,19 @@
 <?php
-$tasks_json = 'http://localhost/php-todo-list-json/api/tasks.json';
-
-$datas = file_get_contents($tasks_json);
-
 header('Content-Type: application/json');
 
-echo $datas;
+$source_path = __DIR__ . '/../tasks.json';
+
+$json_data = file_get_contents($source_path);
+
+$tasks = $json_data;
+
+$new_task = $_POST['task'] ?? '';
+if($new_task){
+    $tasks = json_decode($json_data, true);
+    $tasks[] = $new_task;
+    $tasks = json_encode($tasks);
+} 
+
+
+
+echo $tasks;
