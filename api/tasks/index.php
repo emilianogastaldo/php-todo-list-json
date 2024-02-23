fc94f59d00e946b8b11752b2c84e2845b11bbd57
emilianogastaldo <emiliano0303@gmail.com>
2024-02-23 11:05:51 +0100

    added fns

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -7,25 +7,17 @@ $json_data = file_get_contents($source_path);
 
 $tasks = $json_data;
 
-$new_task = $_POST['task'] ?? '';
-if($new_task){
+$task_text = $_POST['task_text'] ?? '';
+if($task_text){
     $tasks = json_decode($json_data, true);
+    $new_task = [
+        'id' => uniqid(),
+        'text' => $task_text,
+        'done' => false
+    ];
     $tasks[] = $new_task;
     $tasks = json_encode($tasks);
     file_put_contents($source_path, $tasks);
 } 
-
-$delete_id = $_POST['id'] ?? '';
-if($delete_id){
-    $tasks = json_decode($json_data, true);
-
-    $tasks = array_filter($tasks, function($task) use($delete_id){
-        return $task['id'] !== $delete_id;
-    });
-
-    $tasks = json_encode($tasks);
-    file_put_contents($source_path, $tasks);
-}
-
 
 echo $tasks;
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const { createApp } = Vue;
-const endpoint = 'http://localhost/php-todo-list-json/api/tasks/index.php';
+const endpoint = 'http://localhost/php-todo-list-json/api/tasks/';
 const app = createApp({
     name: 'Todo List',
     data: () => ({
@@ -12,7 +12,7 @@ const app = createApp({
         })
     },
     computed: {
-        newTask() { return { id: Date.now(), text: this.inputTask, done: false } }
+        newTask() { return { id: Date.now(), text: this.inputTask, done: '' } }
     },
     methods: {
         addNewTask() {
@@ -23,17 +23,42 @@ const app = createApp({
         },
         addNewTaskAxios() {
             if (!this.inputTask) return;
-            const data = { 'task': this.newTask };
-            const config = { headers: { 'Content-Type': 'multipart/form-data' } };
-            axios.post(endpoint, data, config).then(res => { this.tasks = res.data });
+            const data = { 'task_text': this.inputTask };
+            this.fetchApi(endpoint, data);
             this.inputTask = '';
+
+            // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+            // axios.post(endpoint, data, config).then(res => { this.tasks = res.data });
         },
         deleteTask(id) {
             // this.tasks = this.tasks.filter(task => task.id !== id);
-            const data = { 'id': id };
+            const data = { id };
+            this.fetchApi(`${endpoint}delete/`, data);
+
+            // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+            // axios.post(`${endpoint}delete/`, data, config).then(res => { this.tasks = res.data });
+        },
+        toggleDone(id) {
+            // task.done = !task.done;
+            const data = { id };
+            this.fetchApi(`${endpoint}toggle/`, data);
+
+            // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+            // axios.post(`${endpoint}toggle/`, data, config).then(res => { this.tasks = res.data });
+
+            // Un'altra maniera per mandare i dati senza il config
+            // 
+            // const data = new FormData();
+            // data.append(id,id);
+            // axios.post(`${endpoint}toggle/`, data).then(res => { this.tasks = res.data });
+
+        },
+
+        // unifico le funzioni:
+        fetchApi(endpoint, data) {
             const config = { headers: { 'Content-Type': 'multipart/form-data' } };
             axios.post(endpoint, data, config).then(res => { this.tasks = res.data });
-        },
+        }
     }
 });
 
